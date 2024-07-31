b5dfdd515a07f07f75005bb88c49ef1e16975062
narumi <sychoi2008@naver.com>
2024-07-31 23:28:59 +0900

    mzbeeperfront mybeeper check function

--- src/pages/MyBeeper.js
+++ src/pages/MyBeeper.js
@@ -1,17 +1,36 @@
 // 내 삐삐 확인하기
+import axios from "axios";
 import MyHeader from "../components/MyHeader";
 
 import "./css/MyBeeper.css";
+import api from "../utils/api";
+import { useState } from "react";
 
 const MyBeeper = () => {
+  const [myMsg, setMyMsg] = useState("");
+  const getMyMsg = () => {
+    api
+      .get("http://localhost:8080/mybeeper/getmymsg", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setMyMsg(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <div>
         <MyHeader headerback={false} headertext={"내 삐삐 확인하기"} />
       </div>
       <div className="pager_area">
-        <input className="green_screen" />
-        <button className="pager_btn"></button>
+        <input value={myMsg} className="green_screen" readOnly />
+        <button onClick={getMyMsg} className="pager_btn"></button>
       </div>
     </div>
   );
